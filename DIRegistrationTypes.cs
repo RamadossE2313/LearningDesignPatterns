92964a83df3d2b8a38eff1a23cc15f77e6f1cd6f
RamadossElumalai <Ramadossrohit2264@gmail.com>
2024-08-04 08:04:12 +0530

    Added DI Registration Types

---
+++ DIRegistrationTypes.cs
@@ -0,0 +1,19 @@
+﻿using LearningDesignPatterns.FactoryPattern;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+// adding services to the servicecollection using class names
+services.AddScoped(typeof(XmlReportGenerator));
+// adding services to the servicecollection using class names using strongly typed
+services.AddScoped<XmlReportGenerator>();
+
+// adding services to the servicecollection using interface and class names
+services.AddScoped(typeof(IReportGenerator), typeof(XmlReportGenerator));
+// adding services to the servicecollection using interface and class names using strongly typed
+services.AddScoped<IReportGenerator, XmlReportGenerator>();
+
+// registering service using func
+services.AddScoped(svc =>
+{
+    return new XmlReportGenerator();
+});
